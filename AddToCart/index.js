06b9432e21fb2e0875e0954323c31f31ccbbb0a5
Tomas <134685836+Tomasonic@users.noreply.github.com>
2023-08-16 10:01:27 +0200

    Add To Cart app

---
+++ AddToCart/index.js
@@ -0,0 +1,7 @@
+let inputFieldEl = document.getElementById("input-field")
+let addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function(){
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
