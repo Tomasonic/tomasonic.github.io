0ec939a4802740062b1b3aa07c25d4ce4ac43e7e
Tomas <134685836+Tomasonic@users.noreply.github.com>
2023-09-05 15:39:07 +0200

    code update
    code update

--- AddToCart/index.js
+++ AddToCart/index.js
@@ -1,7 +1,33 @@
-let inputFieldEl = document.getElementById("input-field")
-let addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+ 
+
+const appSettings = {
+    databaseURL: "https://realtime-database-7b2ad-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function(){
     let inputValue = inputFieldEl.value
-    console.log(inputValue)
+
+    push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()
+    appendItemToShoppingListEl(inputValue)
+
 })
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue){
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
