5770d83d65159c19b59679b11a2f65d3dd0733fe
unknown <joalt29@gmail.com>
2023-09-22 18:22:44 +0200

    Update script.js
    adding detailed comments

--- Javascript learning/9.Expanding Cards/script.js
+++ Javascript learning/9.Expanding Cards/script.js
@@ -1,14 +1,20 @@
-const panel_div = document.querySelectorAll('.panel')
+// Get all elements with the class 'panel' and store them in a variable
+const panel_div = document.querySelectorAll('.panel');
 
+// Add a click event listener to each 'panel' element
 panel_div.forEach((panel) => {
     panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+        // Call the 'removeActiveClasses' function to remove 'active' class from all panels
+        removeActiveClasses();
 
+        // Add the 'active' class to the clicked panel, making it expand
+        panel.classList.add('active');
+    });
+});
+
+// Function to remove the 'active' class from all 'panel' elements
 function removeActiveClasses() {
-    panel_div.forEach(panel => { panel.classList.remove('active')
-
-    })
+    panel_div.forEach(panel => {
+        panel.classList.remove('active');
+    });
 }
